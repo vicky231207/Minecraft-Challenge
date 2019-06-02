847e6a6898437c04b475bdd0b62dd7c51342ab91
Alles met Programmeren <51288664+Alles-met-Programmeren@users.noreply.github.com>
2019-06-02 14:48:39 +0200

    een vloer Floor

--- Minecraft/sketch.js
+++ Minecraft/sketch.js
@@ -1,7 +1,12 @@
 function setup() {
-	createCanvas(windowWidth, windowHeight);
+  createCanvas(400, 400);
 }
 
 function draw() {
-
+  background(0,200,255);
+  for(let i = 0;i < 10;i++) {
+   for(let j = 0;j < 3;j++) {
+     rect(i*40,j*40+280,40,40);
+   }
+  }
 }
