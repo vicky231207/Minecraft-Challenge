1b2e87e014a6b84f8769e04f5e57f53596b19c53
Alles met Programmeren <51288664+Alles-met-Programmeren@users.noreply.github.com>
2019-06-03 07:40:58 +0200

    Een bewegende blok erbij maken

--- Minecraft/sketch.js
+++ Minecraft/sketch.js
@@ -1,9 +1,31 @@
+let pos;
+
 function setup() { 
-  createCanvas(400, 400,WEBGL); 
+  createCanvas(600, 400,WEBGL); 
+  pos = createVector();
 } 
 function draw() { 
   background(0,200,255); 
   translate(-200,-200,-200); 
+  pos.y = 240
+  if(keyIsPressed) {
+   if(key == 'a') {
+     pos.x -= 1;
+   }
+    if(key == 'd') {
+     pos.x += 1; 
+    }
+    if(key == 'w') {
+     pos.z -= 1; 
+    }
+    if(key == 's') {
+      pos.z += 1;
+    }
+  }
+  push();
+  translate(pos);
+  box(40);
+  pop();
   for(let i = 0;i < 10;i++) { 
     for(let j = 0;j < 3;j++) { 
       for(let k = 0;k < 10;k++) { 
